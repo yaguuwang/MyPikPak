bd4a05579c59b960da976344ac8452cfe8848c7d
Yaguu Wang <yaguuwang@gmail.com>
2023-01-27 19:30:09 -0800

    init commit

---
+++ MyPikPak/MyPikPakApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyPikPakApp.swift
+//  MyPikPak
+//
+//  Created by Haoyu Wang on 1/27/23.
+//
+
+import SwiftUI
+
+@main
+struct MyPikPakApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
