f3a940fe89d52350c262ba86acf48b3d65ab37e0
Yaguu Wang <yaguuwang@gmail.com>
2023-01-29 21:59:53 -0800

    adopt mvvm for sign in view

---
+++ MyPikPak/View/SignInView.swift
@@ -0,0 +1,33 @@
+//
+//  ContentView.swift
+//  MyPikPak
+//
+//  Created by Haoyu Wang on 1/27/23.
+//
+
+import SwiftUI
+
+struct SignInView: View {
+    @StateObject private var signInVM = SignInViewModel()
+    
+    var body: some View {
+        VStack {
+            TextField("User Name", text: $signInVM.request.username)
+                .textFieldStyle(.roundedBorder)
+            
+            SecureField("Password", text: $signInVM.request.password)
+                .textFieldStyle(.roundedBorder)
+            
+            Button("Sign In") {
+                signInVM.fetchSignIn()
+            }
+        }
+        .padding()
+    }
+}
+
+struct SignInView_Previews: PreviewProvider {
+    static var previews: some View {
+        SignInView()
+    }
+}
---
+++ MyPikPak/ViewModel/SignInViewModel.swift
@@ -0,0 +1,36 @@
+//
+//  SignInViewModel.swift
+//  MyPikPak
+//
+//  Created by Haoyu Wang on 1/29/23.
+//
+
+import SwiftUI
+import Combine
+
+class SignInViewModel: ObservableObject {
+    @Published var request = SignInRequstBody()
+    @Published var signedIn = false
+    
+    private var requests = Set<AnyCancellable>()
+    
+    func fetchSignIn() {
+        let encoder = JSONEncoder()
+        var request = URLRequest(url: SignIn.url)
+        request.httpMethod = "POST"
+        request.httpBody = try? encoder.encode(self.request)
+        URLSession.shared.dataTaskPublisher(for: request)
+            .map(\.data)
+            .decode(type: SignIn.self, decoder: JSONDecoder())
+            .replaceError(with: .default)
+            .receive(on: DispatchQueue.main)
+            .sink(receiveValue: store)
+            .store(in: &requests)
+    }
+    
+    private func store(_ signIn: SignIn) {
+        guard signIn != .default else { return }
+        signIn.saveToUserDefault()
+        signedIn = true
+    }
+}
