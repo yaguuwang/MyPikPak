94c3e6b6dd435e7a9ff4ad3da2bed4e2c6c79c80
Yaguu Wang <yaguuwang@gmail.com>
2023-02-04 20:04:59 -0800

    add binding helper util

---
+++ MyPikPak/Extension/Binding+.swift
@@ -0,0 +1,20 @@
+//
+//  Binding+.swift
+//  MyPikPak
+//
+//  Created by Haoyu Wang on 1/30/23.
+//
+
+import SwiftUI
+
+extension Binding {
+    func onChange(_ handler: @escaping () -> Void) -> Binding<Value> {
+        Binding(
+            get: { self.wrappedValue },
+            set: { newValue in
+                self.wrappedValue = newValue
+                handler()
+            }
+        )
+    }
+}
