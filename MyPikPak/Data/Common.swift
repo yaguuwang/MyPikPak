57956ce60521c81029136fe23ccc9fd5a1b7787e
Yaguu Wang <yaguuwang@gmail.com>
2023-02-04 20:03:53 -0800

    add device id

--- MyPikPak/Data/Common.swift
+++ MyPikPak/Data/Common.swift
@@ -10,4 +10,5 @@ import Foundation
 struct Common {
     static let client_id = "YcrttD06T9PIkqAY"
     static let client_secret = "A3zfcmfNEeyTH0pX2k4GNg"
+    static let device_id = "879f8d1d6660c5290a89ad95652b69d6"
 }
